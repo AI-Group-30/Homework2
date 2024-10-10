b8ac0d6256d323a24a78ee23eea4c53f2b3244c5
HDTHREE <hd3nni4@gmail.com>
2024-10-10 16:05:54 -0400

    comments

--- sort.pl
+++ sort.pl
@@ -1,54 +1,65 @@
 /*swap the first two elements if they are not in order*/
 swap([X, Y|T], [Y, X | T]):-
-    Y < X. % TODO THIS WAS NOT HOW ANCA HAD IT. SHE HAD IT WRONG. HERS BROKE FOR LISTS WITH DUPLICATES.
+    Y < X.
 
 /*swap elements in the tail*/
 swap([H|T], [H|T1]):-
     swap(T, T1).
 
-/* Comment describing bubbleSort */
+/*
+    bubbleSort sorts a list by repeatatively swapping adjacent elements if in the wrong order.  
+    When a swap is made do a recursive call on the new (1 element more sorted) list.
+    Otherwise, no swap was needed and the list is sorted
+*/
 bubbleSort(L,SL):-
     swap(L, L1), % at least one swap is needed
     !,
     bubbleSort(L1, SL).
-bubbleSort(L, L). % here, the list is already sorted
+bubbleSort(L, L). % Base case, the list is already sorted
 
-/* Comment describing ordered */
-ordered([]).
+/*
+    Holds true if a list is in decending order
+    Base Case: Assumes empty and 1 element lists are ordered.
+    Otherwise, the head must be less than the next and the remainder of the list must be sorted
+*/
+ordered([]). 
 ordered([_X]).
 ordered([H1, H2|T]):-
-    H1 =< H2,
-    ordered([H2|T]).
+    H1 =< H2,  % Head less than or equal to the next
+    ordered([H2|T]). % Sort remainder
 
-/*Comment describing insert(E, SL, SLE) ...*/
-/*Comment describing the 1st clause of insert ...*/
+/*
+    An element (next minimum value) inserted into an emtpy list.
+    Otherwise an element is inserted into a list as the new head.
+    Elements should be inserted in order and the tail should be ordered
+*/
 insert(X, [],[X]).
 insert(E, [H|T], [E,H|T]):-
-    ordered(T),
-    E =< H,  % TODO: look at this
+    ordered(T),  % Checks our new sorted list is sorted
+    E =< H,   % Maintains the new element is inserted in order i.e. less than the first value
     !.
 
-/*Comment describing the 2nd clause of insert ...*/
+/* Ignores the head from the previous clause and insert the element into the ordered list i.e. not as the head value. */
 insert(E, [H|T], [H|T1]):-
     ordered(T),
     insert(E, T, T1).
 
-/* Comment describing insertionSort */
+/* Repeatedly call insert to sort elements in a sorted partition of the list. Increase the partition until its the whole list. */
 insertionSort([], []).
 insertionSort([H|T], SORTED) :-
     insertionSort(T, T1),
     insert(H, T1, SORTED).
 
-/* Comment to describe meregeSort... */
-mergeSort([], []). % The empty list is sorted
-mergeSort([X], [X]):-!.
+/* Divides a list in half and sorts each of the remaining lists. Then uses merge to merge each sorted half. */
+mergeSort([], []). % The empty list is sorted.
+mergeSort([X], [X]):-!.  % List with 1 element is sorted
 mergeSort(L, SL):-
     split_in_half(L, L1, L2),
     mergeSort(L1, S1),
     mergeSort(L2, S2),
     merge(S1, S2, SL).
 
-/* Comment to describe split_in_half...*/
+/* Splits a list down the middle into 2 lists.*/
 intDiv(N,N1, R):- R is div(N,N1).
 split_in_half([], _, _):-!, fail.
 split_in_half([X],[],[X]).
@@ -58,9 +69,9 @@ split_in_half(L, L1, L2):-
     length(L1, N1),
     append(L1, L2, L).
 
-/* Comment describing merge(S1, S2, S) */
-merge([], L, L). % comment
-merge(L, [],L). % comment
+/* Combies 2 sorted lists into 1 sorted list containing each element of the initial lists. */
+merge([], L, L). % Merging with an empty list result in the inital list.
+merge(L, [],L). % Same is true here. This is necessary for the definition of S1=[] and S2=[].
 merge([H1|T1],[H2|T2],[H1|T]):-
     H1 < H2,
     merge(T1,[H2|T2],T).
@@ -68,7 +79,7 @@ merge([H1|T1], [H2|T2], [H2|T]):-
     H2 =< H1,
     merge([H1|T1], T2, T).
 
-/* Comment describing split for quickSort */
+/* Splits a list into 2 smaller lists at a given index `x` in the list.*/
 split(_, [],[],[]).
 split(X, [H|T], [H|SMALL], BIG):-
     H =< X,
@@ -77,7 +88,7 @@ split(X, [H|T], SMALL, [H|BIG]):-
     X =< H,
     split(X, T, SMALL, BIG).
 
-/* Comment describing quickSort */
+/* Selects a pivot and then recursively sorts around the pivot until each partition is sorted. */
 quickSort([], []).
 quickSort([H|T], LS):-
     split(H, T, SMALL, BIG),
@@ -85,7 +96,11 @@ quickSort([H|T], LS):-
     quickSort(BIG, B),
     append(S, [H|B], LS).
 
-/* Comment describing hybridSort */
+/*
+    Combies 1 of a small sort (insertion & bubble) with a big sort (merge & quick) to more efficiently sort memebers of list.
+    Base case: 0 or 1 lenght lists are always sorted.
+    Otherwise: Select the big or large sorting algorithm respective to the value of the threshold and the lenght of the list.
+*/
 hybridSort(_, _, _, [], []).
 hybridSort(_, _, _, [X], [X]).
 hybridSort( SMALLALG, BIGALG, LIST, THRESHOLD, SLIST):-
